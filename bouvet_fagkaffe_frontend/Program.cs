3857518ab4bdb2c1369e657e8a64f16de888ff22
Audun Ims Torseth <audun.torseth@bouvet.no>
2024-02-18 13:20:33 +0100

    feat: boilerplate config with sustainsys

--- bouvet_fagkaffe_frontend/Program.cs
+++ bouvet_fagkaffe_frontend/Program.cs
@@ -1,6 +1,9 @@
 using bouvet_fagkaffe_frontend.Components;
 using bouvet_fagkaffe_repository.Context;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Sustainsys.Saml2;
+using Sustainsys.Saml2.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,30 @@ builder.Services.AddDbContext<FagkaffeContext>(options =>
     options.UseSqlServer(connection);
 });
 
+// Adding Authentication with SAML
+builder.Services.AddAuthentication(sharedOptions =>
+{
+    sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    sharedOptions.DefaultChallengeScheme = "Saml2";
+}).AddSaml2(options =>
+{
+    options.SPOptions.EntityId = new EntityId("705b4634-d459-4a4d-911d-e94fdc0d395a");
+    options.IdentityProviders.Add(
+        new IdentityProvider(
+            new EntityId("https://sts.windows.net/27e71101-1d69-483a-91d0-34b1c6356c88/"), options.SPOptions)
+        {
+            MetadataLocation = "https://login.microsoftonline.com/27e71101-1d69-483a-91d0-34b1c6356c88/federationmetadata/2007-06/federationmetadata.xml"
+        });
+})
+.AddCookie();
+
+// Adding Authorization with SAML
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,6 +66,11 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
